8274765be49ae1a9ee9973d30ea8b8c97526054a
xclusivesmash <sbumatshiane916@gmail.com>
2023-08-11 22:17:05 +0200

    Initial commit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    last_digit = number % 10 if number > 0 else number % (-10)
+    print("{}".format(abs(last_digit)), end='')
+    return abs(last_digit)
