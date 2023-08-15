6e157f20a378deae012e06c90709142b6bcba64e
xclusivesmash <sbumatshiane916@gmail.com>
2023-08-15 02:13:28 +0200

    Fixed __import__()

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
-
 if __name__ == "__main__":
-    import add_0
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
