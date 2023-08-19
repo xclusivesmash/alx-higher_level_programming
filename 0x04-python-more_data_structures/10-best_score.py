1b83a2b36c58b43d41c2bbeea95ea3d7c68268a0
xclusivesmash <sbumatshiane916@gmail.com>
2023-08-19 11:46:05 +0200

    Resolved checker problem

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,10 +3,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    key_ = ""
-    maxi = 0
-    for k, v in a_dictionary.items():
-        if v > maxi:
-            maxi = v
-            store = k
-    return store
+    return max(a_dictionary, key=a_dictionary.get)
