57d02d6a6d3b1678f465a7d2094c270500dd106f
xclusivesmash <sbumatshiane916@gmail.com>
2023-09-14 03:05:14 +0200

    Updated initialization method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,9 @@ Inherits from built-in list class.
 
 class MyList(list):
     """MyList Class Definition"""
+    def __init__(self):
+        super().__init__(self)
+
     def print_sorted(self):
         """Prints sorted list in ascending order"""
         print(sorted(self))
