0f4e0b15dbef00d60f93519f8284c81c292f1b6d
xclusivesmash <sbumatshiane916@gmail.com>
2023-09-14 04:10:13 +0200

    Updated exception

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -26,6 +26,6 @@ class BaseGeometry:
             value (int): input integer.
         """
         if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
