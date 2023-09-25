d406ceacd5e324b1229b7b57587aa15a7b40c4a7
xclusivesmash <sbumatshiane916@gmail.com>
2023-09-25 13:05:19 +0200

    Initial commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module: base"""
+
+
+class Base:
+    """Base Class Definition
+    Args:
+        id (int): input integer.
+    Methods:
+        __init__(self, id=None)
+    """
+    # constants
+    __nb_objects = 0
+
+    # initialization
+    def __init__(self, id=None):
+        """Initialization method.
+        Attributes:
+            id (int): identification for each instance."""
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
