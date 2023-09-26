761c32485428e45c5c8e8fb6163b4c76f8ea6ebb
xclusivesmash <sbumatshiane916@gmail.com>
2023-09-26 08:45:37 +0200

    Updated the __str__ method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,8 +12,15 @@ class Square(Rectangle):
         id (int): input integer.
     Methods:
         __init__(self, size: int, x=0, y=0, id=None)
+        __str__(self)
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialization method."""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String representation of the object."""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}" \
+            .format(self.__class__.__name__, self.id, self.x, self.y,
+                    self.width)
