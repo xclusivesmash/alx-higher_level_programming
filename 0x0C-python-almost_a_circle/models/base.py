48d8fe4e0a8a6298a467862b96c64a3d716f9e3c
xclusivesmash <sbumatshiane916@gmail.com>
2023-09-26 09:18:38 +0200

    Updated base class
    Added serialization method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module: base"""
+import json
 
 
 class Base:
@@ -22,3 +23,10 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Serialization method: json"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
