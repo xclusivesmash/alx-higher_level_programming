398a97b33ddfa85da1996353cd447d63b773ef74
xclusivesmash <sbumatshiane916@gmail.com>
2024-01-13 10:34:10 +0200

    Updated query statement

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,4 +4,4 @@
 -- on mysql server.
 
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY "user_0d_1_pwd";
-GRANT ALL PRIVILEDGES ON *.* TO `user_0d_1`@`localhost`;
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
