f3d841eb0eb27cb09e56ff50201918be33b5efda
xclusivesmash <sbumatshiane916@gmail.com>
2024-01-13 10:39:51 +0200

    Initial commit

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- Granting privileges.
+--
+-- create a new databse: hbtn_0d_2
+-- create a new user: user_0d_2
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY "user_0d_2_pwd";
+GRANT SELECT ON hbtn_0d_2.* TO `user_0d_2`@`localhost`;
+FLUSH PRIVILEGES;
