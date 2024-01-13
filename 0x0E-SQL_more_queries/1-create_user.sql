f7e8656219283d5e82cde74fc0fc07317b793cbf
xclusivesmash <sbumatshiane916@gmail.com>
2024-01-13 10:32:55 +0200

    Updated the CREATE statement

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
 -- user to be added: user_0d_1 with all priviledges
 -- on mysql server.
 
-CREATE IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY "user_0d_1_pwd";
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY "user_0d_1_pwd";
 GRANT ALL PRIVILEDGES ON *.* TO `user_0d_1`@`localhost`;
