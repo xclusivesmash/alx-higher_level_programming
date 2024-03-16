305ac5abaccbcf8f96944b7b1cf04c5ad731b860
xclusivesmash <sbumatshiane916@gmail.com>
2024-03-16 20:30:54 +0200

    Initial commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * Description: dealing with cmd args.
+ */
+
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
