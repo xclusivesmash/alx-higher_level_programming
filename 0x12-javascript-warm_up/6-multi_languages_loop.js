ec4ca6aaa23f4cac1ea90a5efa596e8a558f1ad1
xclusivesmash <sbumatshiane916@gmail.com>
2024-03-16 23:15:52 +0200

    Updated code

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const data of myArray) {
   console.log(data);
 }
