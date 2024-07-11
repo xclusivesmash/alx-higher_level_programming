01e8f42e20467156e4439887aad6028fa6f4708b
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-11 12:54:08 -0700

    Initial commit

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const argv = process.argv;
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = argv.map(Number).slice(2, argv.length).sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
+}
