b79d13da3eaef70ccde975b2ba920ee0053ed328
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-17 13:57:46 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+/*
+* Ractangle class creation
+*/
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let m = '';
+      for (let j = 0; j < this.width; j++) {
+        m = m + 'X';
+      }
+      console.log(m);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
