9d1df1faadca27af57408374d031f418073f3409
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-19 12:30:13 -0700

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module: 11-model_state_insert
+Description: Adds a new state objects to a
+database named hbtn_0e_6_usa.
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """
+    list all State objects from the database
+    hbtn_0e_6_usa
+    """
+    username_ = argv[1]
+    passwd_ = argv[2]
+    db = argv[3]
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}"
+        .format(username_, passwd_, db), pool_pre_ping=True)
+    # start session
+    Session = sessionmaker(bind=engine)
+    # start session
+    session = Session()
+
+    # start of work
+    newState = State(name="Louisiana")
+    session.add(newState)
+    session.commit()
+    print("{}".format(newState.id))
+    # end of work
+
+    # close session
+    session.close()
