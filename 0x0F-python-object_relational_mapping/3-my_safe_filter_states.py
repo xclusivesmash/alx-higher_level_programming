1f533ca9732727f673d9fbcc084be743e68a8ec6
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-19 01:52:42 -0700

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module: 3-my_safe_filter_states
+Description: List states where input matches name.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """
+    List all states in the database in ASC order.
+    The input must match name parameter.
+    """
+    arguments = sys.argv
+    user_ = arguments[1]
+    passwd_ = arguments[2]
+    database = arguments[3]
+    name_ = arguments[-1]
+    db = MySQLdb.connect(
+        host="localhost", user=user_, passwd=passwd_, db=database)
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY %(name)s \
+                ORDER BY states.id ASC", {'name': name_})
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
