33709fe58f03bdb3f7dbb58dbe1e8acfba87b842
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-19 02:17:49 -0700

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module: 4-cities_by_state
+Description: List cities from a given database.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """
+    List all cities in the database in ASC order.
+    """
+    arguments = sys.argv
+    user_ = arguments[1]
+    passwd_ = arguments[2]
+    database = arguments[3]
+    db = MySQLdb.connect(
+        host="localhost", user=user_, passwd=passwd_, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
