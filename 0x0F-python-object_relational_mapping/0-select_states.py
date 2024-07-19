6e1d17bb3c508293d3c77dc5b821a2b093831b47
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-19 01:13:55 -0700

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module: 0-select_states
+Description: Manipulating databases using MySQLdb
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    arguments = sys.argv
+    user_ = arguments[1]
+    passwd_ = arguments[2]
+    database = arguments[-1]
+    db = MySQLdb.connect(
+        host="localhost", user=user_, passwd=passwd_, db=database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
