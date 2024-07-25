f9b59134f0b407f24913db3280d3c5ed7e1f1605
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-25 13:32:57 -0700

    Fixed spelling

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,12 +3,12 @@
 Module: 4-hbtn_status
 Description: fetches https://alx-intranet.hbtn.io/status
 """
-import request
+import requests
 
 
 if __name__ == "__main__":
     URL = "https://alx-intranet.hbtn.io/status"
-    response = request.get(URL)
+    response = requests.get(URL)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
