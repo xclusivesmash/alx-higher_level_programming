94da630f76c78a3ae2d80cfa41400f1e9ad7b796
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-30 14:12:33 -0700

    Initial commit

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+/* prints the number of movies where
+the character “Wedge Antilles” is present.
+*/
+
+const request = require('request');
+let num = 0;
+// query using url
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    content.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(18)) {
+          num = num + 1;
+        }
+      });
+    });
+    console.log(num);
+  }
+});
