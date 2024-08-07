2505e0bab5beece139504dedbb5e57284a6eae29
xclusivesmash <sbumatshiane916@gmail.com>
2024-08-07 14:12:52 -0700

    Updated code

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,6 @@
+#!/usr/bin/env node
 /* updates content and responds with a callback function */
+const $ = window.$;
 $('div#red_header').click(function () {
     $('header').css('color', '#FF0000');
   });
