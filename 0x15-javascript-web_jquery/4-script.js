54a31ec51d41d131ca479db19a1a1a9367282a70
xclusivesmash <sbumatshiane916@gmail.com>
2024-08-07 14:10:10 -0700

    Added intepreter

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,4 +1,7 @@
+#!/usr/bin/env node
 /* toggles the class of header element */
+const $ = window.$;
 $('div#toggle_header').click(function () {
     $('header').ToggleClass('red');
+    $('header').toggleClass('green');
   });
